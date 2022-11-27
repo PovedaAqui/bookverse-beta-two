94014623f07773fbc2de225f4b8e24faf4fdab0c
luis.poveda93 <luis.poveda9321@gmail.com>
2022-11-28 00:14:54 +0100

    APIs working

--- pages/api/get-my-books.js
+++ pages/api/get-my-books.js
@@ -1,6 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Cors from 'cors';
+import initMiddleware from '../lib/init-middleware';
 
-export default function handler(req, res) {
+// Initialize the cors middleware
+const cors = initMiddleware(
+    // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+    Cors({
+      // Only allow requests with GET, POST and OPTIONS
+      methods: ['GET', 'POST', 'OPTIONS'],
+    })
+  )
+
+export default async function handler(req, res) {
+    
+    // Run cors
+    await cors(req, res)
 
     const address = req.query.address;
     const chain = process.env.NEXT_PUBLIC_CHAIN;
