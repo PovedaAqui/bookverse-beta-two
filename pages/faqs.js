5addb1a0167b1f8d6457136b6e87cfaf2dd73b57
luis.poveda93 <luis.poveda9321@gmail.com>
2023-02-11 00:30:34 +0100

    build errors solved

--- pages/faqs.js
+++ pages/faqs.js
@@ -19,7 +19,7 @@ const Faqs = () => {
                     <div className="mb-10">
                         <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                             <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg>
-                            What do you mean "cBooks"?
+                            What do you mean cBooks?
                         </h3>
                         <p className="text-gray-500 dark:text-gray-400">cBooks stands for Collectible Books, that is a digital book backed by blockchain technology. It comes with new incredibles opportunities for readers, authors, and publishers.</p>
                     </div>
@@ -35,7 +35,7 @@ const Faqs = () => {
                             <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg>
                             How it works?
                         </h3>
-                        <p className="text-gray-500 dark:text-gray-400">You can buy cBooks in the Store page using your email or connecting your digital wallet such as Metamask, Coinbase Wallet, etc. After you have bought a cBook, you'll be able to read it in the Shelf page, or using any other compatible application</p>
+                        <p className="text-gray-500 dark:text-gray-400">You can buy cBooks in the Store page using your email or connecting your digital wallet such as Metamask, Coinbase Wallet, etc. After you have bought a cBook, youll be able to read it in the Shelf page, or using any other compatible application</p>
                     </div>
                     <div className="mb-10">
                         <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
@@ -56,9 +56,9 @@ const Faqs = () => {
                         <div className="mb-10">
                             <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                                 <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg>
-                                I'm a author or publisher, how can I publish my book with you?
+                                I am a author or publisher, how can I publish my book with you?
                             </h3>
-                            <p className="text-gray-500 dark:text-gray-400">We'll be listing a form very soon.</p>
+                            <p className="text-gray-500 dark:text-gray-400">Well be listing a form very soon.</p>
                         </div>
                         <div className="mb-10">
                             <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
