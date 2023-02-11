c6edc5ba66f07608f8f96f5920407df3a974ccd2
luis.poveda93 <luis.poveda9321@gmail.com>
2023-02-11 17:54:05 +0100

    Editing head data

---
+++ pages/head.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Head () {
+    return (
+        <>
+            <Head>
+                <title>Bookverse</title>
+                <meta name="description" content="he first Digital Collectible Books (cBooks) Marketplace" />
+            </Head>
+        </>
+    );
+}
