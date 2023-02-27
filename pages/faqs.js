b694fe3f3fa109fe6e9384955d20cd754165f70f
luis.poveda93 <luis.poveda9321@gmail.com>
2023-02-27 20:12:58 +0100

    faqs page updated

--- pages/faqs.js
+++ pages/faqs.js
@@ -62,7 +62,7 @@ const Faqs = () => {
                                 <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg>
                                 I am a author or publisher, how can I publish my book with you?
                             </h3>
-                            <p className="text-gray-500 dark:text-gray-400">Well be listing a form very soon.</p>
+                            <p className="text-gray-500 dark:text-gray-400">Send us an email to <a href="mailto:authors@bookversexyz.com" className="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline" target="_blank" rel="noreferrer">authors@bookversexyz.com</a>.</p>
                         </div>
                         <div className="mb-10">
                             <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
@@ -76,7 +76,7 @@ const Faqs = () => {
                                 <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg>
                                 Is this an open-source project?
                             </h3>
-                            <p className="text-gray-500 dark:text-gray-400">Yes, you can find more information about license in our <a href="https://github.com/PovedaAqui/bookverse" className="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline" target="_blank" rel="noreferrer">Github repo</a>.</p>
+                            <p className="text-gray-500 dark:text-gray-400">Yes, you can find more information about license in our <a href="https://github.com/PovedaAqui/bookverse-beta-two" className="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline" target="_blank" rel="noreferrer">Github repo</a>.</p>
                         </div>
                         <div className="mb-10">
                             <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
