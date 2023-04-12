a3e099d825178d2f807b09ea258232a520f40461
luis.poveda93 <luis.poveda9321@gmail.com>
2023-04-12 23:45:15 +0200

    faqs page updated

--- pages/faqs.js
+++ pages/faqs.js
@@ -37,6 +37,31 @@ const Faqs = () => {
                     <div className="mb-10">
                         <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                             <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg>
+                            Why do you need NFTs?
+                        </h3>
+                        <p className="text-gray-500 dark:text-gray-400">
+                        Bookverse items are tied to a Non-Fungible Token (NFT) that proves ownership and authenticity. Like a physical book, 
+                        the NFT ensures that this item is unique and belongs to you. It can be displayed on your digital shelf, 
+                        passed down to others, or moved outside the platform. The NFT connects you to the original creators and anyone who has owned it. 
+                        It's scarce, unalterable, and can't be lost or destroyed, 
+                        making it a safe and valuable addition to your collection.
+                        </p>
+                    </div>
+                    <div className="mb-10">
+                        <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                            <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg>
+                            Why cBooks are better than traditional eBooks?
+                        </h3>
+                        <p className="text-gray-500 dark:text-gray-400">
+                        One major benefit of cBooks in Bookverse is their link to an NFT, which unlocks a plethora of new features for the buyer, 
+                        such as proof of ownership, authenticity, and a direct connection to the author's community. Another unique advantage is the limited availability of cBooks, 
+                        making them a valuable and scarce digital item that stands out from the endless supply of e-books available on other platforms. 
+                        This digital scarcity adds to the unique value of cBooks, making them a worthwhile investment for collectors and readers alike.
+                        </p>
+                    </div>
+                    <div className="mb-10">
+                        <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                            <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg>
                             How it works?
                         </h3>
                         <p className="text-gray-500 dark:text-gray-400">You can buy cBooks in the Store page using your email or connecting your digital wallet such as Metamask, Coinbase Wallet, etc. After you have bought a cBook, youll be able to read it in the Shelf page, or using any other compatible application</p>
@@ -62,7 +87,18 @@ const Faqs = () => {
                                 <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg>
                                 I am a author or publisher, how can I publish my book with you?
                             </h3>
-                            <p className="text-gray-500 dark:text-gray-400">Send us an email to <a href="mailto:authors@bookversexyz.com" className="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline" target="_blank" rel="noreferrer">authors@bookversexyz.com</a>.</p>
+                            <p className="text-gray-500 dark:text-gray-400">Please, complete the <a href="https://docs.google.com/forms/d/e/1FAIpQLSeHcOgIeCmub0idGjFFYgzWt1-i2UHMy0wmz4m-hhdunSwIhA/viewform" className="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline" target="_blank" rel="noreferrer">Authors Onboard form</a>.</p>
+                        </div>
+                        <div className="mb-10">
+                            <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                <svg className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg>
+                                How could I win money as an author using Bookverse?
+                            </h3>
+                            <p className="text-gray-500 dark:text-gray-400">
+                            Make money with Bookverse by pre-selling your book excerpt for a pre-order, earning 96.5% of the list price at launch, receiving royalties from secondary sales, and selling additional features/services together with the utilities given by your books. Bookverse only takes a 3.5% cut, which is much less than traditional publishers or Amazon itself.
+                            Remember that the most benefits you give to your readers (nft holders), the more valuable your Books (NFTs) will be in the secondary market, creating a bigger additional passive income from royalties.
+                            As your cBook gains more popularity and people become interested in it, the demand for your book increases, which can lead to an appreciation in its value. This means that initial holders who have already read the book can sell it for a profit, while you, as the author, continue to earn royalties from its sales. Moreover, the new owner who acquires the cBook benefits from the unique utilities that it provides, such as proof of ownership, authenticity, and a direct connection to the author's community. Therefore, owning a cBook offers both financial and non-financial benefits, making it a valuable addition to anyone's collection.
+                            </p>
                         </div>
                         <div className="mb-10">
                             <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
